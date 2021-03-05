b7b564c1bd3ec4829a4e9dd13dd3b749cd1bab6f
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-05 11:10:54 +0900

    updated model

--- back/src/model/__init__.py
+++ back/src/model/__init__.py
@@ -2,13 +2,16 @@ from src import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
 from .user import User
+from .lending import Lending
 
 
 __all__ = [
     User,
+    Lending,
 ]
