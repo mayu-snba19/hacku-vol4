f581388ed15fcffe8469a147a65f7f26b4c5cd01
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-06 17:08:13 +0900

    docstringを追加した

--- back/src/domain/entity/user_entity.py
+++ back/src/domain/entity/user_entity.py
@@ -3,6 +3,20 @@ from dataclasses import dataclass, field
 
 @dataclass
 class UserEntity:
+    """
+    ユーザーのエンティティ
+
+    Attributes
+    ----------
+    id: str
+        ユーザーのLINE ID
+    name: str
+        ユーザー名
+    picture_url: str
+        アイコン画像のURL
+    status_message: str
+        ステータスメッセージ
+    """
     id: str
     name: str = field(default=None)
     picture_url: str = field(default=None)
