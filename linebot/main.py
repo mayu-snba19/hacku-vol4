936045e118d3ec74c6eb958435a733a39067aaba
mayu <63080020+mayu-snba19@users.noreply.github.com>
2021-03-06 10:36:06 +0900

    change file

--- linebot/main.py
+++ linebot/main.py
@@ -43,9 +43,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
+  profile = line_bot_api.get_profile(user_id) #ユーザidを取得
+  line_bot_api.replyMessage(event.replyToken, {
+    type: 'image',
+    originalContentUrl: 'profile.picture_url',
+    previewImageUrl: 'profile.picture_url'
+  });
+  s=profile.display_name+"「"+event.message.text+"」"
+  line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=s))
 
 if __name__ == "__main__":
 #    app.run()
