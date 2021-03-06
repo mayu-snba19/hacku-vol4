ebfc3d3eae98a77ecfa9a75a74ee67735a7eb0f0
mayu <63080020+mayu-snba19@users.noreply.github.com>
2021-03-06 10:51:49 +0900

    change file

--- linebot/main.py
+++ linebot/main.py
@@ -43,7 +43,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World!'))
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
 #    app.run()
