fefa944bc1f814f31d157a99af0320f2d3737994
kazuki-komori <komokazu.nankai@gmail.com>
2021-03-07 01:55:29 +0900

    feature: lending handlers

--- back/src/api/service/lending.py
+++ back/src/api/service/lending.py
@@ -1,18 +1,105 @@
+from typing import Tuple
+
+
 class LendingService:
-    """
-    貸出機能の処理系
+    """ 貸出機能の処理系
     """
 
     def __init__(self, token):
         self.token = token
 
-    def register(self, content: str, deadline: str) -> (int, str):
+    def register_lending(self, content: str, deadline: str) -> Tuple[int, str]:
         """ 貸出情報の登録
-        :param content: 貸出内容
-        :type deadline: str
-        :param deadline: 返却日
-        :type deadline: str
+        Parameters
+        ----------
+        content: str
+            貸出内容
+        deadline: str
+            返却日
 
-        :return: 貸出ID, created_at
+        Returns
+        -------
+        lendingId: int
+            貸出ID
+        created_at: str
+            登録時間
         """
         return 1, "2020/1/1"
+
+    def register_borrower(self, lending_id: int) -> Tuple[str, str, str]:
+        """ 借りた人の登録
+        Parameters
+        ----------
+        lending_id: int
+            貸出ID
+
+        Returns
+        -------
+        content: str
+            貸出内容
+        deadline: str
+            返却日
+        ownerName: str
+            貸した人の名前
+        """
+        return "貸出内容", "返却日", "貸した人の名前"
+
+    def get_owner_lending(self) -> list:
+        """ 貸したもの一覧
+        Returns
+        -------
+        lendingList: list
+            lendingId: str
+                貸出ID
+            content: str
+                貸出内容
+            deadline: str
+                返却日
+            borrowerName: str
+                借りた人の名前
+        """
+        return [{
+                    'lendingId': "貸出id",
+                    'content': "貸したもの",
+                    'deadline': "期限",
+                    'borrowerName': "借りた人の名前"
+                }]
+
+    def get_borrower_lending(self) -> list:
+        """ 借りたもの一覧
+       Returns
+       -------
+       lendingList: list
+           lendingId: str
+               貸出ID
+           content: str
+               貸出内容
+           deadline: str
+               返却日
+           borrowerName: str
+               借りた人の名前
+       """
+        return [{
+            'lendingId': "貸出id",
+            'content': "貸したもの",
+            'deadline': "期限",
+            'borrowerName': "借りた人の名前"
+        }]
+
+    def register_lending_return(self):
+        """ 返却報告
+        Parameters
+        ----------
+        lending_id: int
+            貸出ID
+
+        Returns
+        -------
+        content: str
+            貸出内容
+        deadline: str
+            返却日
+        ownerName: str
+            借りた人の名前
+        """
+        return "貸出内容", "返却日", "借りた人の名前"
--- back/src/test/api/test_lending.py
+++ back/src/test/api/test_lending.py
@@ -2,8 +2,46 @@ from src.test import client
 from flask import json
 
 
+# 正常系
 def test_register_lending():
     data = json.dumps({"accessToken": "sample1234", "content": "hogefuga", "deadline": "2021/1/1"})
-    response = client.post("/api/lending", data=data, content_type='application/json')
+    response = client.post(
+        "/api/lending",
+        data=data,
+        content_type='application/json',
+        headers={"Authorization": "bearer sampleToken"}
+    )
+    assert response.status_code == 200
+
+
+def test_register_borrower():
+    response = client.put(
+        "/api/lending/1",
+        headers={"Authorization": "bearer sampleToken"}
+    )
+    assert response.status_code == 200
+
+
+def test_get_owner_lending():
+    response = client.get(
+        "/api/owner/lending",
+        headers={"Authorization": "bearer sampleToken"}
+    )
+    assert response.status_code == 200
+
+
+def test_get_borrower_lending():
+    response = client.get(
+        "/api/borrower/lending",
+        headers={"Authorization": "bearer sampleToken"}
+    )
+    assert response.status_code == 200
+
+
+def test_register_lending_return():
+    response = client.delete(
+        "/api/lending/1",
+        headers={"Authorization": "bearer sampleToken"}
+    )
     assert response.status_code == 200
 
