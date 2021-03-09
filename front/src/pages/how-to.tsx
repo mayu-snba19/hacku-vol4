0ca6ae812ae47a2b2bde7fed958e3e0efd59929e
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2021-03-09 21:40:20 +0900

    add template

---
+++ front/src/pages/how-to.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Meta from '~/components/Meta'
+
+const HowTo = () => {
+  return (
+    <>
+      <Meta title="使い方" />
+      <div>使い方説明</div>
+    </>
+  )
+}
+
+export default HowTo
