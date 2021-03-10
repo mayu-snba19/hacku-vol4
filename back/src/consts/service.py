a1594c57a2f7a4382dd134cd10301f03df70177f
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-10 23:03:52 +0900

    change webhook url

--- back/src/consts/service.py
+++ back/src/consts/service.py
@@ -1,3 +1,3 @@
-slack_webhook_url = 'https://hooks.slack.com/services/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'
+slack_webhook_url = 'https://hooks.slack.com/services/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'
 slack_channel = '#そてそてすずめ_notify'
 slack_icon_url = 'https://avatars.slack-edge.com/2021-03-10/1848451967060_13a50ec1f6494a50b368_48.jpg'
