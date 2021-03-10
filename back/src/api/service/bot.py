019b80b2f047ece790263ab944ad286c1fe0c2de
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-10 12:37:58 +0900

    返却のリマインドを行うメソッドを作成した

--- back/src/api/service/bot.py
+++ back/src/api/service/bot.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 
@@ -5,9 +6,11 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+    MessageEvent, TextMessage, TextSendMessage, FlexSendMessage
 )
 
+from src.consts.line import random_messages
+from src.consts.system import root_path
 from src.data import LendingRepositoryImpl, UserRepositoryImpl
 from src.domain.use_case import LendingUseCase
 
@@ -20,10 +23,8 @@ class BotService:
 
         @self.handler.add(MessageEvent, message=TextMessage)
         def handle_message(event: MessageEvent):
-            # deadline_lending_list = lending_use_case.fetch_deadline_lending_list()
-            # for borrower_id, lendings in deadline_lending_list.items():
-
             request_text: str = event.message.text
+            print(request_text)
             split_request_text = request_text.split()
 
             if len(split_request_text) is not 2:
@@ -69,16 +70,34 @@ class BotService:
                     TextSendMessage(text='「はい」か「いいえ」で答えて欲しいチュン。')
                 )
 
-            # with open('./confirm_message.json') as f:
-            #     confirm_message = json.load(f)
-            #     self.line_bot_api.reply_message(
-            #         event.reply_token,
-            #         FlexSendMessage(alt_text='hogeさんに貸したpiyo返ってきたチュン？', contents=confirm_message)
-            #     )
-
     def _response_random(self, reply_token: str):
-        random_message = ['チュン！', 'チュンチュン！', 'メッセージありがとチュン！', 'やっほーだチュン！']
-        self.line_bot_api.reply_message(reply_token, TextSendMessage(text=random.choice(random_message)))
+        self.line_bot_api.reply_message(
+            reply_token,
+            TextSendMessage(text=random.choice(random_messages))
+        )
 
     def handle_hook(self, body, signature):
         self.handler.handle(body, signature)
+
+    def send_message_for_deadline_lendings(self):
+        deadline_lending_list = self.lending_use_case.fetch_deadline_lending_list()
+
+        with open(f"{root_path}/src/api/service/confirm_message.json") as f:
+            base_contents = json.load(f)
+
+        for owner_id, lendings in deadline_lending_list.items():
+            messages = []
+
+            for lending in lendings:
+                self.lending_use_case.start_confirming_returned(lending.id)
+                contents = base_contents.copy()
+                # コールバックのエンドポイントに送信されるデータの末尾に、空白区切りで貸借りidを追加する
+                contents['footer']['contents'][0]['action']['text'] += f" {lending.id}"
+                contents['footer']['contents'][1]['action']['text'] += f" {lending.id}"
+
+                messages.append(FlexSendMessage(
+                    alt_text=f"{lending.borrower_name}さんに貸した{lending.content}帰ってきたチュン？",
+                    contents=contents
+                ))
+
+            self.line_bot_api.push_message(owner_id, messages)
