6c55c44db552b90682e5503fcf5cb4b6ef5c9af6
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2021-03-11 10:47:45 +0900

    フレンド登録解除の実装をした

--- back/src/data/__init__.py
+++ back/src/data/__init__.py
@@ -1,7 +1,9 @@
 from .user_repository_impl import UserRepositoryImpl
 from .lending_repository_impl import LendingRepositoryImpl
+from .friend_repository_impl import FriendRepositoryImpl
 
 __all__ = [
     "UserRepositoryImpl",
     "LendingRepositoryImpl",
+    "FriendRepositoryImpl"
 ]
--- back/src/data/friend_repository_impl.py
+++ back/src/data/friend_repository_impl.py
@@ -1,3 +1,5 @@
+from sqlalchemy import or_, and_
+
 from src import db
 from src.domain.repository import FriendRepository
 from src.model import Friend, User
@@ -31,4 +33,23 @@ class FriendRepositoryImpl(FriendRepository):
         return [user_1.name, user_2.name]
 
     def unregister_friend(self, user_id_1: str, user_id_2: str) -> [str, str]:
-        pass
+        db.session.query(Friend) \
+            .filter(
+            or_(
+                and_(Friend.user_id == user_id_1, Friend.friend_id == user_id_2),
+                and_(Friend.user_id == user_id_2, Friend.friend_id == user_id_1),
+            )
+        ) \
+            .delete()
+
+        db.session.commit()
+
+        user_1 = db.session.query(User.name) \
+            .filter(User.id == user_id_1) \
+            .first()
+
+        user_2 = db.session.query(User.name) \
+            .filter(User.id == user_id_2) \
+            .first()
+
+        return [user_1.name, user_2.name]
